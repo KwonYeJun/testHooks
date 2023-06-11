7654662fd534bce6e13700891af7067516cc0d9a
gwonyejun <kyjuni1059@gmail.com>
2023-06-11 18:42:12 +0900

    주석 달기

--- my-app/src/timer.tsx
+++ my-app/src/timer.tsx
@@ -8,7 +8,7 @@ const Timer = (props : testType) => {
   const [state, setState] = useState<string>(''); // 예시로 state의 타입을 string으로 지정했습니다.
 
   useEffect(() =>{
-
+// 디펜던시에 빈 값을 넣어 처음 마운팅 될 떄 불러 오지만 그 뒤에 clear를 해주어서 종료를 할 수 있게끔 작업을 해보았따.
     const timer = setInterval(() => {
       console.log("timer 돌아가는중..");
     }, 1000);
@@ -19,10 +19,7 @@ const Timer = (props : testType) => {
     }
   },[]);
 
-  useEffect(() => {
-    // 이펙트 동작
-  }, []);
-
+ 
   return (
     <div>
      <span>타이머를 시작 합니다 콘솔을 보세요</span>

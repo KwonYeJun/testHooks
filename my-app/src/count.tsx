38d2ddb3598da660514879a23519b8ebdde03386
gwonyejun <kyjuni1059@gmail.com>
2023-06-11 17:43:27 +0900

    간단하게 useState를 사용 하여 버튼 클릭시 1씩 증가하게 만들어 주었다.

--- my-app/src/count.tsx
+++ my-app/src/count.tsx
@@ -1,12 +1,18 @@
 import React, {useState, useEffect} from 'react';
 
 export default function count(){
+const [test, settest] = useState(1);
+const countAdd = () => {
+settest(test + 1);
+}
+
+return (
+  <div>
+    <button onClick={countAdd}></button>
+    <span>카운팅 획수 : {test}</span>
+  </div>
+)
 
 
 
-
-
-
-
-  
 }
